1628b0a58b36e3a369714281e2d529b6c3583db4
leahma1208 <leahma1208@gmail.com>
2021-11-29 17:32:20 -0800

    Changes

--- .history/app/comps/TaskComp/task_20211129172729.js
+++ .history/app/comps/TaskComp/task_20211129172729.js
@@ -56,7 +56,7 @@ const Input2 = styled.input`
 const Day = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 40px 20px 10px 20px;
+  margin: 20px 20px 10px 20px;
 `;
 const Head = styled.div`
   font-size: 25px;
@@ -128,7 +128,6 @@ const Details = styled.span`
 font-size: 14px;
 color:#3E3D3D;
 font-weight: 400;
-margin-left: 10px;
 `
 
 
@@ -296,7 +295,7 @@ const TaskComp = ({ display = "", onClick = () => {} }) => {
           </Day>
 
           <Day>
-            <Head className="opensans">Points <Details className="opensans">Choose points for this task</Details></Head>
+            <Head className="opensans">Points</Head>
             <ButtonCont>
               {pointButtons.map((points) => (
                 <PtsButton
@@ -316,7 +315,7 @@ const TaskComp = ({ display = "", onClick = () => {} }) => {
           </Day>
 
           <Day>
-            <Head className="opensans">Members  <Details className="opensans">Assign members for this task</Details></Head>
+            <Head className="opensans">Members</Head>
             <MemWrap>
               {roommates.map((roommate) => (
                 <MemCont key={roommate.id}>
